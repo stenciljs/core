fd7be9740f018613b89bede779095f86c8e2d2fc
Christian Bromann <git@bromann.dev>
2025-04-22 14:16:17 -0700

    fix unit tests

--- src/runtime/render.ts
+++ src/runtime/render.ts
@@ -1,5 +1,5 @@
 import type * as d from '../declarations';
-import { renderVdom } from "./vdom/vdom-render";
+import { renderVdom } from './vdom/vdom-render';
 
 /**
  * Method to render a virtual DOM tree to a container element.
@@ -19,17 +19,17 @@ import { renderVdom } from "./vdom/vdom-render";
  * @param vnode - The virtual DOM tree to render
  * @param container - The container element to render the virtual DOM tree to
  */
-export function render(vnode: any, container: Element) {
+export function render(vnode: d.VNode, container: Element) {
   const cmpMeta: d.ComponentRuntimeMeta = {
     $flags$: 0,
     $tagName$: container.tagName,
-  }
+  };
 
   const ref: d.HostRef = {
     $flags$: 0,
     $cmpMeta$: cmpMeta,
     $hostElement$: container as d.HostElement,
-  }
+  };
 
   renderVdom(ref, vnode);
 }
