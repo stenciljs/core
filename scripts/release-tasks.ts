e5a9bd1852251ef7e19342e57ec51a15ac75486a
Christian Bromann <git@bromann.dev>
2025-05-28 14:01:38 -0700

    fix(scripts): populate failing NPM publish command

--- scripts/release-tasks.ts
+++ scripts/release-tasks.ts
@@ -4,6 +4,7 @@ import Listr, { ListrTask } from 'listr';
 import { buildAll } from './build';
 import { BuildOptions } from './utils/options';
 import { isPrereleaseVersion, isValidVersionInput, SEMVER_INCREMENTS, updateChangeLog } from './utils/release-utils';
+import { stderr } from 'node:process';
 
 /**
  * We have to wrap `execa` in a promise to ensure it works with `Listr`. `Listr` uses rxjs under the hood which
@@ -165,7 +166,12 @@ export async function runReleaseTasks(opts: BuildOptions, args: ReadonlyArray<st
           if (isDryRun) {
             return console.log(`[dry-run] ${cmd} ${cmdArgs.join(' ')}`);
           }
-          return execa(cmd, cmdArgs, { cwd: rootDir });
+          return execa(cmd, cmdArgs, { cwd: rootDir }).then(({ stdout, stderr }) => {
+            if (stderr) {
+              throw new Error(stderr);
+            }
+            console.log(stdout);
+          });
         },
       },
       {
