0898faad8d1416c38816f5933d95d7e29bef444e
Christian Bromann <git@bromann.dev>
2025-07-17 14:20:16 -0700

    tweak

--- test/wdio/wdio.conf.ts
+++ test/wdio/wdio.conf.ts
@@ -67,7 +67,7 @@ export const config: WebdriverIO.Config = {
   // The path of the spec files will be resolved relative from the directory of
   // of the config file unless it's absolute.
   //
-  specs: ['./**/*.test.tsx'],
+  specs: [['./**/*.test.tsx']],
   // Patterns to exclude.
   exclude: [
     // 'path/to/excluded/files'
