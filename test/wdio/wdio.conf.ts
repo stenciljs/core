899bb2a890f2c68507483d73742f07fd67df205c
Christian Bromann <git@bromann.dev>
2025-07-17 12:48:20 -0700

    change maxinstances

--- test/wdio/wdio.conf.ts
+++ test/wdio/wdio.conf.ts
@@ -88,7 +88,7 @@ export const config: WebdriverIO.Config = {
   // and 30 processes will get spawned. The property handles how many capabilities
   // from the same test should run tests.
   //
-  maxInstances: 5,
+  maxInstances: 2,
   //
   // we set this to an empty array here and programmatically add configuration below
   //
