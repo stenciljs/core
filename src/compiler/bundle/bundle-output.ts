19b56d19772b2f113cdc87837305b55eedea0361
Fabrice Tsiava <35506438+Fa3ris@users.noreply.github.com>
2025-07-28 11:41:40 +0200

    fix(bundle): remove post order of node-resolve (#6353)
    * fix(bundle): remove post order of node-resolve
    
    node-resolve resolveId hook has been moved to the end of the plugin chain,
    so other plugins that were meant to be called after resolve some modules
    incorrectly
    
    fixes: #6335
    
    * style: format
    
    ---------
    
    Co-authored-by: Fabrice Tsiava <fabrice.tsiava@contentsquare.com>

--- src/compiler/bundle/bundle-output.ts
+++ src/compiler/bundle/bundle-output.ts
@@ -3,7 +3,7 @@ import rollupJsonPlugin from '@rollup/plugin-json';
 import rollupNodeResolvePlugin from '@rollup/plugin-node-resolve';
 import rollupReplacePlugin from '@rollup/plugin-replace';
 import { createOnWarnFn, isString, loadRollupDiagnostics } from '@utils';
-import { PluginContext, rollup, RollupOptions, TreeshakingOptions } from 'rollup';
+import { type ObjectHook, PluginContext, rollup, RollupOptions, TreeshakingOptions } from 'rollup';
 
 import type * as d from '../../declarations';
 import { lazyComponentPlugin } from '../output-targets/dist-lazy/lazy-component-plugin';
@@ -70,16 +70,18 @@ export const getRollupOptions = (
   nodeResolvePlugin.resolve = async function () {
     // Investigate if we can use this to leverage Stencil's in-memory fs
   };
-  // @ts-expect-error - handler is defined
+
+  assertIsObjectHook(nodeResolvePlugin.resolveId);
+  // remove default 'post' order
+  nodeResolvePlugin.resolveId.order = null;
   const orgNodeResolveId = nodeResolvePlugin.resolveId.handler;
-  // @ts-expect-error - handler is defined
   const orgNodeResolveId2 = (nodeResolvePlugin.resolveId.handler = async function (importee: string, importer: string) {
     const [realImportee, query] = importee.split('?');
-    // @ts-ignore
     const resolved = await orgNodeResolveId.call(
       nodeResolvePlugin as unknown as PluginContext,
       realImportee,
       importer,
+      // @ts-expect-error - missing required properties but works in practice
       {},
     );
     if (resolved) {
@@ -183,3 +185,7 @@ const getTreeshakeOption = (config: d.ValidatedConfig, bundleOpts: BundleOptions
       : false;
   return treeshake;
 };
+
+function assertIsObjectHook<T>(hook: ObjectHook<T>): asserts hook is { handler: T; order?: 'pre' | 'post' | null } {
+  if (typeof hook !== 'object') throw new Error(`expected the rollup plugin hook ${hook} to be an object`);
+}
