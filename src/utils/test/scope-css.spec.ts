c53f72a7c3a5a78f96b7d043c44a457dcb80ff7b
Thomas Saporito <t.saporito@jobtarget.com>
2025-08-01 14:44:55 -0400

    fix(test) increased the threshold for the scoped-css test to allow for testing environment latency

--- src/utils/test/scope-css.spec.ts
+++ src/utils/test/scope-css.spec.ts
@@ -121,7 +121,7 @@ describe('scopeCSS', function () {
   it('should perform relative fast', () => {
     const now = Date.now();
     scopeCss(exampleComponentCss, 'a', true);
-    expect(Date.now() - now).toBeLessThan(200);
+    expect(Date.now() - now).toBeLessThan(300);
   });
 
   it('should handle complicated selectors', () => {
