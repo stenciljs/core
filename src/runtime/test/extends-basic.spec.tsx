61647b3cd2e465f1811efeeea01245399c49c140
John Jenkins <john.jenkins@nanoporetech.com>
2025-10-01 00:22:52 +0100

    chore: prettier

--- src/runtime/test/extends-basic.spec.tsx
+++ src/runtime/test/extends-basic.spec.tsx
@@ -1,4 +1,4 @@
-import { Component, h,Prop, Watch } from '@stencil/core';
+import { Component, h, Prop, Watch } from '@stencil/core';
 import { newSpecPage } from '@stencil/core/testing';
 
 declare global {
