4aab0fdd8bf30370ea854bf2a4addfcf311cc149
John Jenkins <johnljenkins@hotmail.com>
2026-05-01 12:31:49 +0100

    chore:

--- packages/core/src/server/runner/index.ts
+++ packages/core/src/server/runner/index.ts
@@ -1,10 +1,5 @@
 export { createWindowFromHtml } from './create-window';
-export {
-  ssrDocument,
-  renderToString,
-  serializeDocumentToString,
-  streamToString,
-} from './render';
+export { ssrDocument, renderToString, serializeDocumentToString, streamToString } from './render';
 export { resetSsrDocData } from './window-initialize';
 
 import { setTagTransformer, transformTag } from '../../runtime';
