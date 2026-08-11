bf91f3d28a940a9d7b9a6f2ea1f5d951df71b743
John Jenkins <johnljenkins@hotmail.com>
2026-08-11 21:11:06 +0100

    chore: format

--- test/build/build-size/kitchen-sink/test-bundle-size.js
+++ test/build/build-size/kitchen-sink/test-bundle-size.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const distDir = path.join(__dirname, 'dist', 'loader-bundle', 'bundlesize-kitchen-sink');
 const maxBundleSize = 30 * 1024; // 30KB in bytes (~29KB non-gzipped, ~10KB gzipped)
-  
+
 console.log('\nChecking bundle size (kitchen-sink)...');
 
 const files = fs.readdirSync(distDir);
