28bf1408bc2dc1321a7338b3579f139f134f435f
John Jenkins <johnljenkins@hotmail.com>
2026-08-12 16:55:25 +0100

    chore:

--- packages/core/src/compiler/transformers/static-to-meta/visitor.ts
+++ packages/core/src/compiler/transformers/static-to-meta/visitor.ts
@@ -49,6 +49,10 @@ export const convertStaticToMeta = (
     };
 
     return (tsSourceFile) => {
+      // TEMP DEBUG (windows build-conditionals investigation)
+      console.warn(
+        `[DEBUG convertStaticToMeta] tsSourceFile.fileName="${tsSourceFile.fileName}" dirname="${dirname(tsSourceFile.fileName)}"`,
+      );
       dirPath = dirname(tsSourceFile.fileName);
       moduleFile = getModuleLegacy(compilerCtx, tsSourceFile.fileName);
       resetModuleLegacy(moduleFile);
