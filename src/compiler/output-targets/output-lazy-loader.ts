0f89386251cd275283fb360f3ca36e2603b4078a
John Jenkins <johnljenkins@hotmail.com>
2026-08-20 16:58:41 +0100

    fix(types): defineCustomElements

--- src/compiler/output-targets/output-lazy-loader.ts
+++ src/compiler/output-targets/output-lazy-loader.ts
@@ -74,7 +74,7 @@ export interface CustomElementsDefineOptions {
   ael?: (el: EventTarget, eventName: string, listener: EventListenerOrEventListenerObject, options: boolean | AddEventListenerOptions) => void;
   rel?: (el: EventTarget, eventName: string, listener: EventListenerOrEventListenerObject, options: boolean | AddEventListenerOptions) => void;
 }
-export declare function defineCustomElements(win?: Window, opts?: CustomElementsDefineOptions): void;
+export declare function defineCustomElements(win?: Window, opts?: CustomElementsDefineOptions): Promise<void>;
 /**
  * @deprecated
  */
